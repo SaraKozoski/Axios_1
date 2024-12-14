9965ca4df63f3f29057d88a5d5a1a4b6874fdcdf
SaraKozoski <sarakozoski@gmail.com>
2024-12-14 19:31:01 -0300

    axios.js modificacao

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 const api = axios.create({
   baseURL: 'https://api.themoviedb.org/3/',
   headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MGZiMDE3NTc1NzAyMDFkNDljMjdjZmVmODU1OTEzZCIsIm5iZiI6MTczMjY0MzE4Ni4zMDMwMTMzLCJzdWIiOiI2NzQ2MDdkYzA2NDI0YmRlMjcwNGYyMTEiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.jfrNK63ytn6b3OGZEbRXpZeNaLeXlORHItYDmCIFwC4`,
+    Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MGZiMDE3NTc1NzAyMDFkNDljMjdjZmVmODU1OTEzZCIsIm5iZiI6MTczMjY0Mjc4MC43MzcsInN1YiI6IjY3NDYwN2RjMDY0MjRiZGUyNzA0ZjIxMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uyp5_uLWE-NL8DC6NZpxUcacieSA0gq0baDK_Z2eeeM`,
   },
 });
 
